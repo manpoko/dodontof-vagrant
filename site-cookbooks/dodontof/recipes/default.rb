bde9d8ee49a7a4a01f286de0aacd6348f26f5563
seiji ochiai <roadmang@gmail.com>
2014-04-15 11:44:30 +0900

    add recipe[dodontof] run setting, package install

--- site-cookbooks/dodontof/recipes/default.rb
+++ site-cookbooks/dodontof/recipes/default.rb
@@ -6,3 +6,19 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+%w{git ruby}.each do |pkg|
+    package pkg do
+        action :install
+    end
+end
+
+user "dodontof" do
+    comment "dodontof"
+end
+
+bash "set locale ja" do
+    user "root"
+    code <<-EOH
+    localedef -f UTF-8 -i ja_JP ja_JP.UTF-8
+    EOH
+end
